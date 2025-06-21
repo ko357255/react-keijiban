09cca348d71135fc6e7fcea8a180b3690afb242a
ko357255 <mikanace@outlook.jp>
2025-06-22 03:42:05 +0900

    スレッド作成を追加

--- src/pages/NewThreads.tsx
+++ src/pages/NewThreads.tsx
@@ -1,7 +1,45 @@
 
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function NewThreads() {
+  const [title, setTitle] = useState('');
+
+  const navigate = useNavigate();
+
+  const createThreads = async () => {
+    const url = `https://railway.bulletinboard.techtrain.dev/threads`;
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({ title }),
+      });
+
+      if (!res.ok) {
+        console.error(res.status, res.statusText);
+        return
+      }
+      const result: { id: string, title: string } = await res.json();
+
+      alert(`「${result.title}」を立てました`);
+      // トップページに遷移する
+      navigate('/');
+
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // フォームの送信をキャンセルする
+    createThreads();
+  }
+
+  // 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
   return (
     <main>
       <div>
@@ -15,9 +53,10 @@ function NewThreads() {
         </div>
       </div>
       <div>
-        <form action="post">
-          <input type="text" />
-          <input type="submit" value='決定'/>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">スレッド名</label>
+          <input name='title' type="text" value={title} placeholder='スレッド名' onChange={handleChange} />
+          <input type="submit" value='決定' />
         </form>
       </div>
     </main>
