30d4cf39f16765a84ed4dfb73fc7e60a97088faa
ko357255 <mikanace@outlook.jp>
2025-06-23 12:08:39 +0900

    ローディングとエラーを追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import ThreadItem from '../components/ThreadItem';
 import type { ThreadData } from '../types/types';
+import ThreadItem from '../components/ThreadItem';
+import ThreadTitle from '../components/ThreadTitle';
 
 const Home = () => {
+  // スレッド
   const [threads, setThreads] = useState<ThreadData[]>([]);
+  // エラー null=エラーなし 文字列=エラーあり
+  const [error, setError] = useState<string | null>(null);
+  // ロード中かどうか
+  const [loading, setLoading] = useState<boolean>(true);
+
 
   // スレッド一覧の取得処理
   const fetchThreads = async () => {
-    // ↑ constで定義したほうが良いらしい 
-    //   再代入防止、巻き上げなし（定義前に呼ぶとエラー）
+    // ↑ constで定義すると再代入防止、巻き上げなし（定義前に呼ぶとエラー）
 
     const offset = 0;
     const url = `https://railway.bulletinboard.techtrain.dev/threads?offset=${offset}`;
@@ -20,14 +25,18 @@ const Home = () => {
         // error.message に格納される
         throw new Error(`Error: ${res.statusText}`);
       }
+
       const data: ThreadData[] = await res.json();
       setThreads(data);
-    } catch (error: any) { // エラーの処理
-      console.error(error);
-      
-    } finally {
-      // try-catchで最後に実行される
+    } catch (error) { // エラーの処理
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError('Error: Unknown');
+      }
 
+    } finally { // try-catchで最後に実行される
+      setLoading(false); // ロード中を終わる
     }
   }
 
@@ -35,24 +44,36 @@ const Home = () => {
     fetchThreads();
   }, []); // 初回描写時に実行
 
-
   return (
     <main>
       <div>
-        <div className='threads-title-box'>
-          <h2 className='title'>スレッド一覧</h2>
-          <Link to={'/threads/new'} className='link-box'>
-            <div>
-              スレッドを立てる
-            </div>
-          </Link>
-        </div>
+        <ThreadTitle title='スレッド一覧' toTitle='スレッドを立てる' to='/threads/new' />
         <table className='threads-table'>
           <tbody>
-            {/* thread配列を繰り返し、テーブル化する */}
-            {threads.map((thread) => (
-              <ThreadItem thread={thread} key={thread.id} />
-            ))}
+            {loading ? ( // ローディング中なら
+              <tr>
+                <td>
+                  Loading...
+                </td>
+              </tr>
+            ) : error ? ( // エラーがあるなら
+              <tr>
+                <td className='error'>
+                  {error}
+                </td>
+              </tr>
+            ) : threads.length == 0 ? ( // スレッドが0なら
+              <tr>
+                <td>
+                  スレッドはありません
+                </td>
+              </tr>
+            ) : ( // ではないなら
+              // threds配列を回して、アイテムを作る
+              threads.map((thread) => (
+                <ThreadItem thread={thread} key={thread.id} />
+              ))
+            )}
           </tbody>
         </table>
       </div>
